4ae33e420812c4d5ad829d5eaeae26d0bee9cd49
maamokun <45969559+maamokun@users.noreply.github.com>
2022-07-06 20:55:23 +0900

    使用率等を入れたaboutコマンドに

---
+++ ping.js
@@ -0,0 +1,15 @@
+const ms = require('ms');
+const os = require("os");
+const osutils = require('os-utils');
+const { resolve } = require('path');
+const { callbackify } = require('util');
+
+module.exports = {
+    name: 'about',
+    aliases: ['ping'],
+    utilisation: '{prefix}about',
+
+    execute(client, message) {
+        message.channel.send(`**サーバー応答速度:** ${client.ws.ping}ms 🛰️ \n**CPUモデル:** ${os.cpus()[0].model} :brain: \n**メモリー使用率:** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${os.totalmem} MB :desktop:`);
+    },
+};

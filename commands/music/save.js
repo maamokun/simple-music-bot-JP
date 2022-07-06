61dca7da18f4296bfa04aa76d6d8bb3068532877
maamokun <45969559+maamokun@users.noreply.github.com>
2022-07-06 21:34:01 +0900

    e

--- commands/music/save.js
+++ commands/music/save.js
@@ -9,7 +9,7 @@ module.exports = {
 
         if (!queue || !queue.playing) return message.channel.send(`❌ | 何も再生されていません`);
 
-        message.author.send(`:floppy_disk: | ${queue.current.title} - ${queue.current.author} (${queue.current.url}) | ${int.member.guild.name} より`).then(() => {
+        message.author.send(`:floppy_disk: | ${queue.current.title} - ${queue.current.author} (${queue.current.url}) | ${message.guild.name} より`).then(() => {
             message.channel.send(`✅ | DMに曲の情報を送信しました`);
         }).catch(error => {
             message.channel.send(`:warning: | DMを送信できませんでした`);

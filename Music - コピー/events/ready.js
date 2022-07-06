7d0f5b67c3c3aa5461f520be7c891fbbbbaeddc8
maamokun <45969559+maamokun@users.noreply.github.com>
2022-07-06 18:44:04 +0900

    みすった

--- Music - コピー/events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client) => {
-    console.log(`ログインしました！ ${client.user.username}\n-> ${client.guilds.cache.size} guilds | ${client.users.cache.size} users`);
-
-    client.user.setActivity(client.config.app.playing);
-};
